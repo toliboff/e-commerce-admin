f64677e17ff9d88f70ad40a4e0fff4ec56f5b096
Tolibjon <tolibjohn@gmail.com>
2022-02-25 11:33:07 +0500

    Add widgets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,18 +2,29 @@ import React from 'react'
 import styled from 'styled-components';
 import FeaturedInfo from '../Components/FeaturedInfo';
 import Chart from '../Components/Chart';
-import { userData } from '../data';
+import { members, userData } from '../data';
+import NewMembers from '../Components/NewMembers';
+import LatestTransactions from '../Components/LatestTransactions';
 
 const Container = styled.div`
   flex: 4;
   padding: 10px;
 `;
 
+const Widgets = styled.div`
+  display: flex;
+  gap: 15px;
+  margin: 15px 0;
+`;
 const Home = () => {
   return (
     <Container>
       <FeaturedInfo />
       <Chart data={userData} dataKey='uv' columnName='name' />
+      <Widgets>
+        <NewMembers members={members}/>
+        <LatestTransactions />
+      </Widgets>
     </Container>
   )
 }
