568c458524653ed082494b3b7740821f547e848b
Tolibjon <tolibjohn@gmail.com>
2022-03-01 20:36:38 +0500

    Create form and inputs

--- src/pages/NewUser.jsx
+++ src/pages/NewUser.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
+import styled from 'styled-components'
 
+const Container = styled.div`
+  flex: 4;
+`;
+
+const Title = styled.h2``;
+const  Form = styled.form``;
+const  InputContainer = styled.div``;
+const  Left = styled.div``;
+const  Right = styled.div``;
+const  Label = styled.label``;
+const  Input = styled.input``;
 const NewUser = () => {
   return (
-    <div>NewUser</div>
+    <Container>
+      <Title>Create User </Title>
+      <Form>
+        <InputContainer>
+          <Left>
+            <Label>Username</Label>
+            <Input type='text' />
+            <Label>Email</Label>
+            <Input type='email' />
+            <Label>Phone</Label>
+            <Input type='tel' />
+          </Left>
+          <Right>
+            <Label>Full Name</Label>
+            <Input type='text' />
+            <Label>Password</Label>
+            <Input type='text' />
+            <Label>Address</Label>
+            <Input type='text' />
+            <Label>Active</Label>
+            <Input type='text' />
+
+          </Right>
+        </InputContainer>
+      </Form>
+    </Container>
   )
 }
 
