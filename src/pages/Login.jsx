ae1f3d7a04f161c8547dfd17f7639375220d1947
Tolibjon <tolibjohn@gmail.com>
2022-03-11 22:04:42 +0500

    Add dispatching actions

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,14 +1,28 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import styled from 'styled-components';
+import { login } from '../redux/api';
+
+const Container = styled.div`
+  flex:4;
+`;
 
 const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+
+  const handleClick = (event) => {
+    event.preventDefault();
+    login(dispatch, {username, password})
+  }
 
   return (
-    <div>
-      <input type="text" placeholder="username" />
-      <input type="password" placeholder="password" />
-    </div>
+    <Container>
+      <input type="text" placeholder="username" onChange={(e)=>setUsername(e.target.value)}/>
+      <input type="password" placeholder="password" onChange={(e)=>setPassword(e.target.value)}/>
+      <button onClick={handleClick}> Login </button>
+    </Container>
   )
 }
 
